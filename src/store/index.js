4acc3f8602fd5d5392a3d30738e37f2c472e7a92
Danillo Soares <Danillohss@gmail.com>
2023-03-02 02:48:26 -0300

    First Commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,36 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    products: [],
+    productsInBag: [],
   },
   mutations: {
+    loadProducts(state, products) {
+      state.products = products;
+    },
+    addToBag(state, product) {
+      state.productsInBag.push(product);
+    },
+    removeFromBag(state, productId) {
+      let updatedBag = state.productsInBag.filter(item => productId != item.id);
+      state.productsInBag = updatedBag;
+    }
   },
   actions: {
+    loadProducts({ commit }) {
+      axios.get('https://fakestoreapi.com/products/')
+        .then(response => {
+          commit('loadProducts', response.data)
+        })
+    },
+    addToBag({ commit }, product) {
+      commit('addToBag', product);
+    },
+    removeFromBag({ commit }, productId) {
+      commit('removeFromBag', productId);
+    }
   },
   modules: {
   }
